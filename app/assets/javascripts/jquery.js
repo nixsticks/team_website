4ea5216fbba979b705dc33b2477756b6e25e54d5
Nikki Thean <nikki.thean@gmail.com>
2013-11-11 18:56:29 -0500

    Update CSS, add randomizer jQuery script for Ruby demo page

--- app/assets/javascripts/jquery.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  var $methods = $('#methods .method').hide();
-  $(‘button’).on(‘click’, function() {
-    $methods.eq(Math.random()*$methods.length).show();​
-  });
-})
